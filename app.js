e52654ffdd9622f37478696d8f37a4ce93ef1fce
lemonbirch <dabbassammy@gmail.com>
2024-06-25 14:30:29 -0400

    contributing to app.js

--- app.js
+++ app.js
@@ -18,6 +18,41 @@ app.get("/storychoicebuilder", (req, res) => {
 app.get("/story", (req, res) => {
   res.render("story.ejs");
 });
+
+function makeChoice(choice) {
+    fetch("/continue_story", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ choice: choice }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("story").innerText = data.new_story;
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+
+  function resetStory() {
+    fetch("/reset_story", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("story").innerText = data.new_story;
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
